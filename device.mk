e2c2b2a190bc8a58105bb7649688eff7f69ff76a
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-05-02 05:45:04 +0200

    X01BD: Build overlayed Audio HAL
    * With new asus tfa amp implementation
    
    Change-Id: I3ac4da68f4bba1242ebcc55f61bce20f483ff06c
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -45,7 +45,8 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.common@4.0-util \
     android.hardware.audio@4.0-impl \
     android.hardware.audio.effect@4.0 \
-    android.hardware.audio.effect@4.0-impl
+    android.hardware.audio.effect@4.0-impl \
+    audio.primary.sdm660
 
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_effects.xml \
