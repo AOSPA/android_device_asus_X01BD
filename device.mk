6dc5ef32a77ea9f70ebe61cac6afbef4d55e6864
Jake Weinstein <jake@aospa.co>
2020-06-01 10:29:16 +0000

    X01BD: move Qualcomm priv-app permissions to vendor/pa
    * Some of these are not telephony specific and should be included on all devices.
    
    Change-Id: I0e2eb65c9010db3e43a39d09f0ee3900f66838fb
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- device.mk
+++ device.mk
@@ -100,10 +100,6 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2-service
 
-# Permissions
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/privapp-permissions-qti.xml:system/etc/permissions/privapp-permissions-qti.xml
-
 # QTI Common
 TARGET_COMMON_QTI_COMPONENTS := \
     av \
