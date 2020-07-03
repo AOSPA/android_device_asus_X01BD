26ce721db3f38db059a302f52ce1160eef6c0820
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-07-03 21:20:14 +0200

    X01BD: Drop local config.fs
    * We already have it in vendor/pa
    
    Change-Id: Id313851a742b0aee15f381746287b9534e8e7def
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -115,9 +115,6 @@ TARGET_ENABLE_MEDIADRM_64 := true
 # Encryption
 TARGET_HW_DISK_ENCRYPTION := true
 
-# Filesystem
-TARGET_FS_CONFIG_GEN := $(DEVICE_PATH)/config.fs
-
 # FM
 BOARD_HAVE_QCOM_FM := true
 
