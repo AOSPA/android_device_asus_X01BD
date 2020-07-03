af4f08482fb44dc2e169955f7674573bbadc1c73
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-07-03 22:09:42 +0200

    X01BD: Drop audio pie audio configs
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Change-Id: I49ca2620ac1bfd1958332ba42a8ed5449d316afd

--- device.mk
+++ device.mk
@@ -47,10 +47,6 @@ PRODUCT_PACKAGES += \
     android.hardware.audio.effect@4.0 \
     android.hardware.audio.effect@4.0-impl
 
-PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/configs/audio/audio_effects.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_effects.xml \
-    $(LOCAL_PATH)/configs/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_PRODUCT)/vendor_overlay/$(PRODUCT_TARGET_VNDK_VERSION)/etc/audio_platform_info.xml
-
 # Bluetooth
 PRODUCT_PACKAGES += \
     libbluetooth_qti \
