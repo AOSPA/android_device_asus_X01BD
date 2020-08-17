00bc4ae33e95d4bde9efdc2156deb18cc2efae50
Vaisakh Murali <vaisakhmurali@gmail.com>
2020-08-17 11:41:12 +0530

    Revert "X01BD: Add basic USB HAL that reports no status change"
    This reverts commit 0fab73c9200d2ca47b8e3e6057c644b47bfb92a8.
    
    Signed-off-by: Vaisakh Murali <vaisakhmurali@gmail.com>
    Change-Id: I60b0f3f6116e6138e78d83c75f57314e97dcaf17

--- device.mk
+++ device.mk
@@ -107,10 +107,6 @@ TARGET_COMMON_QTI_COMPONENTS := \
     perf \
     telephony
 
-# USB
-PRODUCT_PACKAGES += \
-	android.hardware.usb@1.0-service.x01bd
-
 # WFD
 PRODUCT_PACKAGES += \
     libaacwrapper \
